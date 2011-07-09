908663283c90dbf5fe0eeb6466b55e14ac59f410
Paul T. Edlefsen <pedlefsen@gmail.com>
2011-07-08 23:06:28 -0700

    Changed the license to the Apache license

--- Random.hpp
+++ Random.hpp
@@ -15,25 +15,24 @@
 #*    This file is part of prolific, a library of useful C++ classes for
 #*    working with genomic sequence data and Profile HMMs.  Please see the
 #*    document CITING, which should have been included with this file.  You may
-#*    use at will, subject to the license (LGPL v3), but *please cite the
+#*    use at will, subject to the license (Apache v2.0), but *please cite the
 #*    relevant papers* in your documentation and publications associated with
 #*    uses of this library.  Thank you!
 #*
 #*    Copyright (C) 2007, 2008, 2011 by Paul T. Edlefsen, Fred Hutchinson Cancer
 #*    Research Center.
 #*
-#*    prolific is free software: you can redistribute it and/or modify it under
-#*    the terms of the GNU Lesser Public License as published by the Free
-#*    Software Foundation, either version 3 of the License, or (at your option)
-#*    any later version.
-#*
-#*    prolific is distributed in the hope that it will be useful, but WITHOUT
-#*    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-#*    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser Public License for
-#*    more details.
-#*
-#*    You should have received a copy of the GNU Lesser Public License along
-#*    with prolific.  If not, see <http://www.gnu.org/licenses/>.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
 #*****************************************************************************/
 
 #if     _MSC_VER > 1000
